6f8c1fea46a7bf2a181fb115103496f2b90e4c48
Javier Ruiz <yosoyjavieruiz@gmail.com>
2024-11-07 18:45:47 -0400

    user ready more a

--- src/screens/ChatIa/config.ts
+++ src/screens/ChatIa/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-    apiKey: "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",
-    apiUrlChat: "https://api.openai.com/v1/chat/completions",
-    apiUrlImage: "https://api.openai.com/v1/images/generations",
-    gptturbo: 'gpt-4-turbo-preview',
+    apiKey: "",
+    apiUrlChat: "",
+    apiUrlImage: "",
+    gptturbo: '',
 };
