c496972a8661a25878177261cee9317d35b9c10e
Andres Felipe <webmastercloudming@gmail.com>
2024-12-06 23:04:34 -0500

    Successfully atesting

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,10 @@
 const IP = '192.168.1.18'; //"192.168.128.20";
 
-const API_URL: string = `http://${IP}:8000`;
+// const API_URL: string = `http://${IP}:8000`;
 // const API = 'https://dovalai-develop.up.railway.app';
 const API = 'https://doval-ai-production.up.railway.app';
 // const API_URL = 'https://back-doval-llc-develop.up.railway.app';
-// const API_URL = 'https://social-media-backend-production-1ad4.up.railway.app';
+const API_URL = 'https://social-media-backend-production-1ad4.up.railway.app';
 // const SOCKET_URL: string = `https://social-media-backend-production-1ad4.up.railway.app`;
 const SOCKET_URL: string = API_URL;
 const CLOUDFRONT = `https://d3bi2z9onfqiyy.cloudfront.net/`;
